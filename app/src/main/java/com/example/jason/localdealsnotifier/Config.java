667da961252f023fd96a2bdbaf380a2d0857e31e
tim <tim.liu@uwaterloo.ca>
2016-10-15 01:47:47 -0400

    Firebase read and write

---
+++ app/src/main/java/com/example/jason/localdealsnotifier/Config.java
@@ -0,0 +1,11 @@
+package com.example.jason.localdealsnotifier;
+
+/**
+ * Created by tim on 15/10/16.
+ */
+
+public class Config {
+    public static final String FIREBASE_URL = "https://hackwestern3-2ebd2.firebaseio.com/";
+
+
+}
--- app/src/main/java/com/example/jason/localdealsnotifier/MainActivity.java
+++ app/src/main/java/com/example/jason/localdealsnotifier/MainActivity.java
@@ -2,12 +2,82 @@ package com.example.jason.localdealsnotifier;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextAddress;
+    private TextView textViewPersons;
+    private Button buttonSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Firebase.setAndroidContext(this);
+
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
+
+        textViewPersons = (TextView) findViewById(R.id.textViewPersons);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating firebase object
+                Firebase ref = new Firebase(Config.FIREBASE_URL);
+
+                //Getting values to store
+                String name = editTextName.getText().toString().trim();
+                String address = editTextAddress.getText().toString().trim();
+
+                //Creating Promotion object
+                Promotion promotion = new Promotion();
+
+                //Adding values
+                promotion.setName(name);
+                promotion.setAddress(address);
+
+                //Storing values to firebase
+                ref.child("Promotion").setValue(promotion);
+
+
+                //Value event listener for realtime data update
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                            //Getting the data from snapshot
+                            Promotion promotion = postSnapshot.getValue(Promotion.class);
+
+                            //Adding it to a string
+                            String string = "Name: "+ promotion.getCompany()+"\nAddress: "+ promotion.getAddress()+"\n\n";
+
+                            //Displaying it on textview
+                            textViewPersons.setText(string);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(FirebaseError firebaseError) {
+                        System.out.println("The read failed: " + firebaseError.getMessage());
+                    }
+                });
+
+            }
+        });
     }
+
+
 }
